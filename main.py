83b51d197f5d35b76b42c58116f5acd0b98f0417
Michael Wydler <michael.wydler@googlemail.com>
2014-05-28 16:39:33 +0200

    Save images as png.

--- main.py
+++ main.py
@@ -64,8 +64,8 @@ def index():
             image = Image.open(os.path.join(TEST_DIR, 'static', 'bresenham', 'solutions', 'rays.bmp')).resize([512, 512])
             diff = ImageChops.difference(solution, image)
 
-            solution.save(os.path.join(RESULT_DIR, 'rays.jpg'), "JPEG", quality=80, optimize=True)
-            diff.save(os.path.join(RESULT_DIR, 'diff-rays.jpg'), "JPEG", quality=80, optimize=True)
+            solution.save(os.path.join(RESULT_DIR, 'rays.png'), "PNG", optimize=True)
+            diff.save(os.path.join(RESULT_DIR, 'diff-rays.png'), "PNG", optimize=True)
 
             lock.release()
 
